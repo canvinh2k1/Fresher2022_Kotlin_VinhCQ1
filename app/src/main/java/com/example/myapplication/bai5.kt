ef780e14c95cd4c54accb465137040125d6b8e31
canvinh2k1 <19020658@vnu.edu.vn>
2022-06-27 10:37:59 +0700

    baitap

--- app/src/main/java/com/example/myapplication/bai5.kt
+++ app/src/main/java/com/example/myapplication/bai5.kt
@@ -6,7 +6,7 @@ fun main() {
     val year : Int
     year = readLine()!!.toInt()
     when (month) {
-        2 -> if (checkYear(year)) println("29")
+        2 -> if (thang2(year)) println("29")
         else println("28")
         4, 6, 9, 11 -> println("30")
         1, 3, 5, 7, 8, 10, 12 -> println("31")
@@ -14,6 +14,6 @@ fun main() {
     }
 }
 
-fun checkYear(year: Int) : Boolean {
+fun thang2(year: Int) : Boolean {
     return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
 }
